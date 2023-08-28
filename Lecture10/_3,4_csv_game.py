ffcd545db4aa25daf7c71453e364db365d2a178b
YuliaDiachenko <140431276+YuliaDiachenko@users.noreply.github.com>
2023-08-28 17:14:07 +0300

    Lecture10

--- Lecture10/_3,4_csv_game.py
+++ Lecture10/_3,4_csv_game.py
@@ -11,9 +11,10 @@ with open('game_simulator.csv', mode='w') as file:
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
     
-    for name in players:
-        score = [random.randint(0,1000) for level in range(100)]
-        writer.writerow({"Player's name": name, "Score": score})
+    for level in range(100):
+        for name in players:
+            score = random.randint(0,1000) 
+            writer.writerow({"Player's name": name, "Score": score})
 
 
 with open('game_simulator.csv', mode='r') as inputfile, open('high_scores.csv', mode='w') as outputfile:
@@ -22,9 +23,29 @@ with open('game_simulator.csv', mode='r') as inputfile, open('high_scores.csv', 
     writer.writeheader()
 
     reader = csv.DictReader(inputfile)
-    reader_dict = [row for row in reader]
+  
+    reader_dict = []
+    max_dict = {}
     
-    for row in reader_dict:
-        max_score = (max(list(map(int, (row["Score"].strip("[]").split(", "))))))
-        writer.writerow({"Player's name": row["Player's name"], "High score": max_score})
+    for row in reader:
+        reader_dict.append(row)
+        max_dict[row["Player's name"]] = 0
+ 
+    
+    for name in max_dict.keys():
+        max = 0 
+        for row in reader_dict:
+            if (name == row["Player's name"]) and (int(max) < int(row["Score"])):
+                max = int(row["Score"])
+                max_dict[row["Player's name"]] = max
+    
+    max_list = list(max_dict.values())
+    max_list.sort(reverse=True)
+  
+    for score in max_list:
+        for name in max_dict.keys():
+            if score == max_dict[name]: 
+                writer.writerow({"Player's name": name, "High score": score})
+                max_dict.pop(name)
+                break
 
